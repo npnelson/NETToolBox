3a699eaee854ea818de5b697c2f26dba8b8284a2
Nicholas P. Nelson <nnelson1@insight.rr.com>
2017-05-26 14:27:07 -0400

    initial commit

---
+++ src/NetToolBox.TestHelpers.EF/EFHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetToolBox.TestHelpers.EF
+{
+    public static class EFHelpers
+    {
+        public static T CreateLocalDbContext<T>(string dbName,bool preserveDB= false) where T : DbContext
+        {
+            var retval = (T)Activator.CreateInstance(typeof(T),GetDbContextOptions<T>(dbName));
+            if (!preserveDB)
+            {
+                retval.Database.EnsureDeleted();
+                retval.Database.EnsureCreated();
+            }
+            return retval;
+
+        }
+
+        private static DbContextOptions<T> GetDbContextOptions<T>(string dbName) where T:DbContext
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<T>();
+            optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database={dbName};Trusted_Connection=True;Connection Timeout=5");         
+            return optionsBuilder.Options;
+
+        }
+    }
+}
---
+++ test/NetToolBox.Tests/EFHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NetToolBox.TestHelpers.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetToolBox.Tests
+{
+    public class EFHelperTests
+    {
+        [Fact]
+        public async Task CreateContextTest()
+        {
+            var context = EFHelpers.CreateLocalDbContext<EFContext>("EFHelperTests");
+            context.EFRecords.Add(new EFRecord { EFValue = "TEST" });
+            await context.SaveChangesAsync();
+
+            var newContext= EFHelpers.CreateLocalDbContext<EFContext>("EFHelperTests",true);
+            var recCount = await newContext.EFRecords.CountAsync();
+
+            recCount.Should().Be(1);
+        }
+    }
+
+    public class EFRecord
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EFRecordID { get; set; }
+        public string EFValue { get; set; }
+    }
+
+    public class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options):base(options)
+        {
+
+        }
+        public DbSet<EFRecord> EFRecords { get; set; }
+    }
+}
