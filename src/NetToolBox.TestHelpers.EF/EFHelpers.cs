cc1d7b7056f748e98b1f5f3486759ac63fbf182a
Nicholas P. Nelson <nnelson1@insight.rr.com>
2017-07-07 10:25:02 -0400

    added to EFHelpers

--- src/NetToolBox.TestHelpers.EF/EFHelpers.cs
+++ src/NetToolBox.TestHelpers.EF/EFHelpers.cs
@@ -7,22 +7,35 @@ namespace NetToolBox.TestHelpers.EF
 {
     public static class EFHelpers
     {
-        public static T CreateLocalDbContext<T>(string dbName,bool preserveDB= false) where T : DbContext
+        public static T CreateLocalDbContext<T>(string dbName, bool preserveDB = false) where T : DbContext
         {
-            var retval = (T)Activator.CreateInstance(typeof(T),GetDbContextOptions<T>(dbName));
+            var retval = (T)Activator.CreateInstance(typeof(T), GetDbContextOptionsImpl<T>(dbName));
             if (!preserveDB)
             {
-                retval.Database.EnsureDeleted();
-                retval.Database.EnsureCreated();
+                CreateDatabase(dbName, retval);
             }
             return retval;
-
         }
 
-        private static DbContextOptions<T> GetDbContextOptions<T>(string dbName) where T:DbContext
+        private static void CreateDatabase<T>(string dbName, T context = null) where T : DbContext
+        {
+            if (context == null)
+            {
+                context = (T)Activator.CreateInstance(typeof(T), GetDbContextOptionsImpl<T>(dbName));
+            }
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+        public static DbContextOptions<T> GetDbContextOptions<T>(string dbName) where T : DbContext
+        {
+            var retval = GetDbContextOptionsImpl<T>(dbName);
+            CreateDatabase<T>(dbName);
+            return retval;
+        }
+        private static DbContextOptions<T> GetDbContextOptionsImpl<T>(string dbName) where T : DbContext
         {
             var optionsBuilder = new DbContextOptionsBuilder<T>();
-            optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database={dbName};Trusted_Connection=True;Connection Timeout=5");         
+            optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database={dbName};Trusted_Connection=True;Connection Timeout=5");
             return optionsBuilder.Options;
 
         }
