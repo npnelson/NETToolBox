ab22f028f751fc3365bcef6d6522ea99ba1d2e13
Nicholas P. Nelson <nnelson1@insight.rr.com>
2017-08-10 16:08:22 -0400

    initial TPL work

---
+++ src/NetToolBox.TPLDataflow/TPLDataflowBlockCreationFunctions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NetToolBox.TPLDataflow
+{
+    public static class TPLDataflowBlockCreationFunctions
+    {
+        //TODO: Handle errors
+        public static IPropagatorBlock<TInput, TOutput>
+     CreateUnorderedTransformBlock<TInput, TOutput>(
+     Func<TInput, Task<TOutput>> func, ExecutionDataflowBlockOptions options)
+        {
+            var buffer = new BufferBlock<TOutput>(options);
+            var action = new ActionBlock<TInput>(
+                async input =>
+                {
+                    var output = await func(input);
+                    await buffer.SendAsync(output);
+                }, options);
+
+            action.Completion.ContinueWith(
+                t =>
+                {
+                    IDataflowBlock castedBuffer = buffer;
+
+                    if (t.IsFaulted)
+                    {
+                        castedBuffer.Fault(t.Exception);
+                    }
+                    else if (t.IsCanceled)
+                    {
+                // do nothing: both blocks share options,
+                // which means they also share CancellationToken
+            }
+                    else
+                    {
+                        castedBuffer.Complete();
+                    }
+                });
+
+            return DataflowBlock.Encapsulate(action, buffer);
+        }
+    }
+}
---
+++ test/NetToolBox.TestHarness/Program.cs
@@ -0,0 +1,36 @@
+﻿using NetToolBox.TPLDataflow;
+using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NetToolBox.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var opts = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 5,BoundedCapacity=5 };
+            var block1 = new UnorderedTransformBlock<int,int>(async x => {
+                Console.WriteLine(x.ToString() + "T1");
+                await Task.Delay(TimeSpan.FromSeconds(x));            
+               return x;
+           }, opts);
+
+           
+            var block2 = new ActionBlock<int>(x => Console.WriteLine(x), opts);           
+            block1.LinkTo(block2);
+            //  Console.WriteLine($"Input Count={block2.InputCount}");
+            block1.SendAsync(10).GetAwaiter().GetResult() ;
+          //  Console.WriteLine($"Input Count={block2.InputCount}");
+            block1.SendAsync(5);
+          //  Console.WriteLine($"Input Count={block2.InputCount}");
+            block1.SendAsync(1).GetAwaiter().GetResult();
+            block1.SendAsync(10).GetAwaiter().GetResult();
+            block1.SendAsync(9).GetAwaiter().GetResult();
+            block1.SendAsync(8).GetAwaiter().GetResult();
+            block1.SendAsync(7).GetAwaiter().GetResult();
+            //  Console.WriteLine($"Input Count={block2.InputCount}");
+            Console.ReadLine();
+        }
+    }
+}
