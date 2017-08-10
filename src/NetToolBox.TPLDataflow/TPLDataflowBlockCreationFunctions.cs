74de975fe2112bcfd842e49a9324d1920e8c64ec
Nicholas P Nelson <nnelson1@insight.rr.com>
2017-08-10 16:19:24 -0400

    Initial TPL Dataflow (#1)
    * initial TPL work
    
    * minor tweak

---
+++ src/NetToolBox.TPLDataflow/TPLDataflowBlockCreationFunctions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NetToolBox.TPLDataflow
+{
+    public static class TPLDataflowBlockCreationFunctions
+    {
+        //TODO: Handle errors
+        public static IPropagatorBlock<TInput, TOutput>
+     CreateUnorderedTransformBlock<TInput, TOutput>(
+     Func<TInput, Task<TOutput>> func, ExecutionDataflowBlockOptions options)
+        {
+            var buffer = new BufferBlock<TOutput>(options);
+            var action = new ActionBlock<TInput>(
+                async input =>
+                {
+                    //TODO: wrap this in a try catch and only log any exceptions thrown and don't send on to next block
+                    try
+                    {
+                        var output = await func(input);
+                        await buffer.SendAsync(output);
+                    }
+                    catch (Exception ex )
+                    {
+
+                        throw;
+                    }
+                  
+                }, options);
+
+            action.Completion.ContinueWith(
+                t =>
+                {
+                    IDataflowBlock castedBuffer = buffer;
+
+                    if (t.IsFaulted)
+                    {
+                        castedBuffer.Fault(t.Exception);
+                    }
+                    else if (t.IsCanceled)
+                    {
+                // do nothing: both blocks share options,
+                // which means they also share CancellationToken
+            }
+                    else
+                    {
+                        castedBuffer.Complete();
+                    }
+                });
+
+            return DataflowBlock.Encapsulate(action, buffer);
+        }
+    }
+}
---
+++ src/NetToolBox.TPLDataflow/UnorderedTransformBlock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NetToolBox.TPLDataflow
+{
+    public sealed class UnorderedTransformBlock<TInput, TOutput> : IPropagatorBlock<TInput, TOutput>
+    {
+        private readonly IPropagatorBlock<TInput, TOutput> _innerBlock;
+
+        public UnorderedTransformBlock(Func<TInput, Task<TOutput> >transform, ExecutionDataflowBlockOptions options)
+        {
+            _innerBlock = TPLDataflowBlockCreationFunctions.CreateUnorderedTransformBlock(transform, options); 
+        }
+       
+        public Task Completion => _innerBlock.Completion;
+
+        public void Complete()
+        {
+            _innerBlock.Complete();
+        }
+
+        public TOutput ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<TOutput> target, out bool messageConsumed)
+        {
+            return _innerBlock.ConsumeMessage(messageHeader, target, out messageConsumed);
+        }
+
+        public void Fault(Exception exception)
+        {
+            _innerBlock.Fault(exception);
+        }
+
+        public IDisposable LinkTo(ITargetBlock<TOutput> target, DataflowLinkOptions linkOptions)
+        {
+            return _innerBlock.LinkTo(target, linkOptions);
+        }
+
+        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, TInput messageValue, ISourceBlock<TInput> source, bool consumeToAccept)
+        {
+            return _innerBlock.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+
+        public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<TOutput> target)
+        {
+            _innerBlock.ReleaseReservation(messageHeader, target);
+        }
+
+        public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<TOutput> target)
+        {
+            return _innerBlock.ReserveMessage(messageHeader, target);
+        }
+    }
+}
