a3dc31086b9720740aa2ea32a2f371be5e2453c9
Nicholas P. Nelson <nnelson1@insight.rr.com>
2018-01-04 15:38:23 -0500

    wip

---
+++ src/NetToolBox.TPLDataflow/ActionBlockPreventDuplicates.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NetToolBox.TPLDataflow
+{
+    public class ActionBlockPreventDuplicates<T>:ITargetBlock<T>
+    {
+        private readonly ActionBlock<T> _innerBlock;
+        private readonly IDataflowBlock _innerDataFlowBlock;
+        private readonly ITargetBlock<T> _innerTargetBlock;
+        private readonly ConcurrentDictionary<T,bool> _dictionary=new ConcurrentDictionary<T,bool>(); //we don't use the value part of the dictionary, but want to ensure we only have one entry
+        public ActionBlockPreventDuplicates(Action<T> action,ExecutionDataflowBlockOptions dataflowBlockOptions)
+        {
+            _innerBlock = new ActionBlock<T>(action, dataflowBlockOptions);
+            _innerDataFlowBlock = (IDataflowBlock)_innerBlock;
+            _innerTargetBlock = _innerBlock;
+        }
+
+        private Func<T, Task> GetActionWrapper(Func<T, Task> action)
+        {
+            var retval = new Func<T, Task>(x => {
+                if (InsertItem(x))
+                {
+                    try
+                    {
+                        return action(x);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        RemoveItem(x);
+                    }
+                }
+                else
+                {
+                    return Task.CompletedTask; //we just want to return a completed task because it is a dup
+                }
+            });
+            return retval;
+        }
+        private Action<T> GetActionWrapper(Action<T> action)
+        {
+            var retval = new Action<T>(x =>
+            {
+                if (InsertItem(x))
+                {
+                    try
+                    {
+                        action(x);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        RemoveItem(x);
+                    }
+                }
+            });
+            return retval;
+        }
+        private bool InsertItem(T item)
+        {
+            var added = _dictionary.TryAdd(item, false);
+            return added;
+        }
+
+        private void RemoveItem(T item)
+        {
+
+        }
+
+        public ActionBlockPreventDuplicates(Func<T,Task> action, ExecutionDataflowBlockOptions dataflowBlockOptions)
+        {
+            _innerBlock = new ActionBlock<T>(action, dataflowBlockOptions);
+            _innerDataFlowBlock = (IDataflowBlock)_innerBlock;
+            _innerTargetBlock = _innerBlock;
+        }
+
+        public Task Completion => _innerBlock.Completion;
+
+        public void Complete()
+        {
+            _innerBlock.Complete();
+        }
+
+         void IDataflowBlock.Fault(Exception exception)
+        {           
+            _innerDataFlowBlock.Fault(exception);
+          
+        }
+
+        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, T messageValue, ISourceBlock<T> source, bool consumeToAccept)
+        {          
+            return _innerTargetBlock.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+    }
+}
---
+++ test/NetToolBox.Tests/DataflowTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NetToolBox.TPLDataflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Xunit;
+
+namespace NetToolBox.Tests
+{
+    public class DataflowTests
+    {
+        [Fact]
+        public async Task ProcessSinglePost()
+        {
+            var fixture = new DataflowTestFixture();
+            await fixture.ActionBlockFixture.SendAsync("file1");
+            fixture.ActionBlockFixture.Complete();
+            await fixture.ActionBlockFixture.Completion;
+            fixture.ProcessedList.Single().Should().Be("file1");
+        }
+        [Fact]
+        public async Task DoesntProcessDuplicates()
+        {
+            var fixture = new DataflowTestFixture();
+            await fixture.ActionBlockFixture.SendAsync("file1");
+            await fixture.ActionBlockFixture.SendAsync("file1");
+            fixture.ActionBlockFixture.Complete();
+            await fixture.ActionBlockFixture.Completion;
+            fixture.ProcessedList.Single().Should().Be("file1");
+        }
+    }
+
+    public class DataflowTestFixture
+    {
+        public readonly ActionBlockPreventDuplicates<string> ActionBlockFixture;
+        public readonly List<string> ProcessedList=new List<string>();
+        private readonly object _listLock = new object();
+        public DataflowTestFixture()
+        {
+            ActionBlockFixture = new ActionBlockPreventDuplicates<string>(x => ProcessItem(x), new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 2, EnsureOrdered = false });
+        }
+        private void ProcessItem(string item)
+        {
+            lock (_listLock)
+            {
+                ProcessedList.Add(item);
+            }
+        }
+    }
+}
