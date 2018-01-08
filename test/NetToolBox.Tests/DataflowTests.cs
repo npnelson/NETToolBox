84a35a8e8f7be6fe2ab3a0a857c3c8da28ab018c
Nicholas P Nelson <nnelson1@insight.rr.com>
2018-01-08 07:54:09 -0500

    wip tests

--- test/NetToolBox.Tests/DataflowTests.cs
+++ test/NetToolBox.Tests/DataflowTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Moq;
 using NetToolBox.TPLDataflow;
 using System;
 using System.Collections.Generic;
@@ -16,36 +17,58 @@ namespace NetToolBox.Tests
         public async Task ProcessSinglePost()
         {
             var fixture = new DataflowTestFixture(false);
+            fixture.MockProcessItem.Setup(x => x.ProcessItem(It.IsAny<string>()));
             await fixture.ActionBlockFixture.SendAsync("file1");
             fixture.ActionBlockFixture.Complete();
             await fixture.ActionBlockFixture.Completion;
-            fixture.ProcessedList.Single().Should().Be("file1");
+            fixture.MockProcessItem.Verify(x=>x.ProcessItem(It.Is<string>(y=>y=="file1")),Times.Once);
         }
         [Fact]
         public async Task DoesntProcessDuplicates()
         {
             var fixture = new DataflowTestFixture(false);
+            fixture.MockProcessItem.Setup(x => x.ProcessItem(It.IsAny<string>()));
             await fixture.ActionBlockFixture.SendAsync("file1");
             await fixture.ActionBlockFixture.SendAsync("file1");
             fixture.ActionBlockFixture.Complete();
             await fixture.ActionBlockFixture.Completion;
-            fixture.ProcessedList.Single().Should().Be("file1");
+            fixture.MockProcessItem.Verify(x => x.ProcessItem(It.Is<string>(y => y == "file1")), Times.Once);
         }
 
         [Fact]
         public async Task DoesntProcessDuplicatesAsync()
         {
             var fixture = new DataflowTestFixture(true);
+            fixture.MockProcessItem.Setup(x => x.ProcessItem(It.IsAny<string>()));
             await fixture.ActionBlockFixture.SendAsync("file1");
             await fixture.ActionBlockFixture.SendAsync("file1");
             fixture.ActionBlockFixture.Complete();
             await fixture.ActionBlockFixture.Completion;
-            fixture.ProcessedList.Single().Should().Be("file1");
+            fixture.MockProcessItem.Verify(x => x.ProcessItem(It.Is<string>(y => y == "file1")), Times.Once);
         }
+        [Fact]
+        public async Task ProcessDuplicateAfterAlreadyProcessesedAsync()
+        {
+            var fixture = new DataflowTestFixture(true);
+            fixture.MockProcessItem.Setup(x => x.ProcessItem(It.IsAny<string>()));
+            await fixture.ActionBlockFixture.SendAsync("file1");
+            await fixture.ActionBlockFixture.SendAsync("file2");
+            await fixture.ActionBlockFixture.SendAsync("file1");
+            fixture.ActionBlockFixture.Complete();
+            await fixture.ActionBlockFixture.Completion;
+            fixture.MockProcessItem.Verify(x => x.ProcessItem(It.Is<string>(y => y == "file1")), Times.Exactly(2));
+            fixture.MockProcessItem.Verify(x => x.ProcessItem(It.Is<string>(y => y == "file2")), Times.Once);
+        }
+    }
+
+    public interface IProcessItem
+    {
+        void ProcessItem(string item);
     }
 
     public class DataflowTestFixture
     {
+        public readonly Mock<IProcessItem> MockProcessItem = new Mock<IProcessItem>();
         public readonly ActionBlockPreventDuplicates<string> ActionBlockFixture;
         public readonly List<string> ProcessedList=new List<string>();
         private readonly object _listLock = new object();
@@ -53,7 +76,7 @@ namespace NetToolBox.Tests
         {
             if (!asyncFixture)
             {
-                ActionBlockFixture = new ActionBlockPreventDuplicates<string>(x => ProcessItem(x), new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 2, EnsureOrdered = false });
+                ActionBlockFixture = new ActionBlockPreventDuplicates<string>(x => MockProcessItem.Object.ProcessItem(x), new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 2, EnsureOrdered = false });
             }
             else
             {
@@ -63,15 +86,15 @@ namespace NetToolBox.Tests
 
         private  Task ProcessItemAsync(string item)
         {
-            ProcessItem(item);
+            MockProcessItem.Object.ProcessItem(item);
             return Task.CompletedTask;
         }
-        private void ProcessItem(string item)
-        {
-            lock (_listLock)
-            {
-                ProcessedList.Add(item);
-            }
-        }
+        //private void ProcessItem(string item)
+        //{
+        //    lock (_listLock)
+        //    {
+        //        ProcessedList.Add(item);
+        //    }
+        //}
     }
 }
