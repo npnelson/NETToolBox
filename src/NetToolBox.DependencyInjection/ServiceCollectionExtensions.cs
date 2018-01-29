a73d83ab0a30aba83fab3717b1eca9ea60ec44ec
Nicholas P. Nelson <nnelson1@insight.rr.com>
2018-01-29 10:50:28 -0500

    added ConfigurePOCO method

---
+++ src/NetToolBox.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static TConfig ConfigurePOCO<TConfig>(this IServiceCollection services, IConfiguration configuration) where TConfig : class, new()
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            var config = configuration.Get<TConfig>();
+            services.AddSingleton(config);
+            return config;
+        }
+
+    }
+
+}
